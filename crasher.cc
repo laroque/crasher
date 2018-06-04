4964b22f014694b6cc56a9f02b08134ed5690325
Ben LaRoque <benjamin.laroque@gmail.com>
2018-06-04 05:52:10 -0700

    crasher now takes a command line arg to specify the time until it crashes

--- crasher.cc
+++ crasher.cc
@@ -1,5 +1,6 @@
 #include <thread>
 #include <chrono>
+#include <sstream>
 #include <iostream>
 
 void print_count()
@@ -13,21 +14,26 @@ void print_count()
     }
 }
 
-void crash()
+void crash( int delay )
 {
     std::cout << "crashing" << std::endl;
-    std::this_thread::sleep_for( std::chrono::seconds( 13 ) );
+    std::this_thread::sleep_for( std::chrono::seconds( delay ) );
     bool* nothing;
     delete nothing;
     delete nothing;
 }
 
-int main()
+int main(int argc, char** argv)
 {
+    int a_delay = 13;
+    if (argc  == 2) {
+        std::istringstream iss( argv[1] );
+        if (!(iss >> a_delay)) {
+            std::cout << "unable to convert argument" << std::endl;
+        }
+    }
     std::thread start_count( print_count );
-    std::thread then_crash( crash );
+    std::thread then_crash( crash, a_delay );
     start_count.join();
     return 0;
 }
-
-
