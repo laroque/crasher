e5a53a19cae81280d29aaad216e13eeb37bf416f
Ben LaRoque <benjamin.laroque@gmail.com>
2018-06-04 07:11:13 -0700

    more debugging prints

--- crasher.cc
+++ crasher.cc
@@ -16,7 +16,7 @@ void print_count()
 
 void crash( int delay )
 {
-    std::cout << "crashing in " << delay << "seconds" << std::endl;
+    std::cout << "crashing in " << delay << " seconds" << std::endl;
     std::this_thread::sleep_for( std::chrono::seconds( delay ) );
     bool* nothing;
     delete nothing;
@@ -25,6 +25,11 @@ void crash( int delay )
 
 int main(int argc, char** argv)
 {
+    std::cout << "input is: ";
+    for (int i=1; i<argc; ++i) {
+        std::cout << argv[i] << " ";
+    }
+    std::cout << std::endl;
     int a_delay = 13;
     if (argc  == 2) {
         std::istringstream iss( argv[1] );
